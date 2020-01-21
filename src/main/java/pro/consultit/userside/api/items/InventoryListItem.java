47301c6cbefc14182e0aed395ec6f16e72820000
Павел Кузин <root@consult-it.pro>
2020-01-21 13:46:33 +0300

    update

--- src/main/java/pro/consultit/userside/api/items/InventoryListItem.java
+++ src/main/java/pro/consultit/userside/api/items/InventoryListItem.java
@@ -2,7 +2,14 @@ package pro.consultit.userside.api.items;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InventoryListItem {
 	private int id;
 
@@ -27,6 +34,7 @@ public class InventoryListItem {
 	private double cost;
 	@JsonProperty("serial_number")
 	private String serialNumber;
+
 	@JsonProperty("inventory_number")
 	private String inventoryNumber;
 
@@ -41,127 +49,4 @@ public class InventoryListItem {
 	@JsonProperty("location_object_id")
 	private int locationObject;
 
-	public InventoryListItem() {
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getCatalogId() {
-		return catalogId;
-	}
-
-	public void setCatalogId(int catalogId) {
-		this.catalogId = catalogId;
-	}
-
-	public int getCatalogSectionId() {
-		return catalogSectionId;
-	}
-
-	public void setCatalogSectionId(int catalogSectionId) {
-		this.catalogSectionId = catalogSectionId;
-	}
-
-	public String getSectionName() {
-		return sectionName;
-	}
-
-	public void setSectionName(String sectionName) {
-		this.sectionName = sectionName;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getMeasure() {
-		return measure;
-	}
-
-	public void setMeasure(String measure) {
-		this.measure = measure;
-	}
-
-	public int getSellerId() {
-		return sellerId;
-	}
-
-	public void setSellerId(int sellerId) {
-		this.sellerId = sellerId;
-	}
-
-	public int getAmount() {
-		return amount;
-	}
-
-	public void setAmount(int amount) {
-		this.amount = amount;
-	}
-
-	public double getCost() {
-		return cost;
-	}
-
-	public void setCost(double cost) {
-		this.cost = cost;
-	}
-
-	public String getSerialNumber() {
-		return serialNumber;
-	}
-
-	public void setSerialNumber(String serialNumber) {
-		this.serialNumber = serialNumber;
-	}
-
-	public String getInventoryNumber() {
-		return inventoryNumber;
-	}
-
-	public void setInventoryNumber(String inventoryNumber) {
-		this.inventoryNumber = inventoryNumber;
-	}
-
-	public String getBarcode() {
-		return barcode;
-	}
-
-	public InventoryListItem setBarcode(String barcode) {
-		this.barcode = barcode;
-		return this;
-	}
-
-	public int getLocationType() {
-		return locationType;
-	}
-
-	public void setLocationType(int locationType) {
-		this.locationType = locationType;
-	}
-
-	public int getLocationSubAccount() {
-		return locationSubAccount;
-	}
-
-	public void setLocationSubAccount(int locationSubAccount) {
-		this.locationSubAccount = locationSubAccount;
-	}
-
-	public int getLocationObject() {
-		return locationObject;
-	}
-
-	public void setLocationObject(int locationObject) {
-		this.locationObject = locationObject;
-	}
 }
