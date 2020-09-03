9c6505f1224be69f7f84c15a4fb0c07fea3aac9a
Павел Кузин <root@consult-it.pro>
2020-09-03 18:57:27 +0300

    1
    Signed-off-by: Павел Кузин <root@consult-it.pro>

--- src/main/java/pro/consultit/userside/api/response/IndexEncapsulatedResponse.java
+++ src/main/java/pro/consultit/userside/api/response/IndexEncapsulatedResponse.java
@@ -2,7 +2,9 @@ package pro.consultit.userside.api.response;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class IndexEncapsulatedResponse<I, T> extends DefaultResponse {
@@ -22,7 +24,13 @@ public class IndexEncapsulatedResponse<I, T> extends DefaultResponse {
 		return data;
 	}
 
-	public void setData(Map<I, T> data) {
-		this.data = data;
+	public void setData(Object data) {
+		if (data instanceof Map) {
+			this.data = (Map<I, T>) data;
+		} else if (data instanceof List && ((List) data).size() == 0) {
+			this.data = Collections.EMPTY_MAP;
+		} else {
+			throw new IllegalArgumentException("Invalid value: " + data);
+		}
 	}
 }
