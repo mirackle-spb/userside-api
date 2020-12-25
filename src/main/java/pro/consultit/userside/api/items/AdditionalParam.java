7525e0e8aba6fec14b5fbf9071df7c70e006f480
Павел Кузин <root@consult-it.pro>
2020-12-25 15:31:16 +0300

    1
    Signed-off-by: Павел Кузин <root@consult-it.pro>

--- src/main/java/pro/consultit/userside/api/items/AdditionalParam.java
+++ src/main/java/pro/consultit/userside/api/items/AdditionalParam.java
@@ -1,10 +1,12 @@
 package pro.consultit.userside.api.items;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AdditionalParam {
 	@JsonProperty("id")
 	private int id;
