868c53a9a30db3093590a8dd40fe814fcc137684
Павел Кузин <root@consult-it.pro>
2021-01-15 13:53:15 +0300

    1
    Signed-off-by: Павел Кузин <root@consult-it.pro>

--- src/main/java/pro/consultit/userside/api/items/task/TaskCatalogType.java
+++ src/main/java/pro/consultit/userside/api/items/task/TaskCatalogType.java
@@ -1,11 +1,13 @@
 package pro.consultit.userside.api.items.task;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TaskCatalogType {
 
